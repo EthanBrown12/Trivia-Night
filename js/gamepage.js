1ff9940ec466c13c8a2dc200d401fea0b1768143
Ethan Brown <ethanb18@hotmail.com>
2020-09-30 17:29:20 +0000

    Fixed some spacing issues and some game instruction as well as clicking on the answer number

--- js/gamepage.js
+++ js/gamepage.js
@@ -1,8 +1,12 @@
 const gameCurrentQuestion = document.getElementById('gameCurrentQuestion');
 let option1 = document.querySelector('.option1');
+let answerNumber1 = document.querySelector('.answer-number1')
 let option2 = document.querySelector('.option2');
+let answerNumber2 = document.querySelector('.answer-number2')
 let option3 = document.querySelector('.option3');
+let answerNumber3 = document.querySelector('.answer-number3')
 let option4 = document.querySelector('.option4');
+let answerNumber4 = document.querySelector('.answer-number4')
 let questionCounter = document.getElementById('questionCounter');
 let currentQuestion = 1;
 let score = document.getElementById('score');
@@ -15,9 +19,13 @@ const modalText = document.getElementById('end-game');
 
 
 option1.addEventListener('click', option1Click)
+answerNumber1.addEventListener('click', answerNumber1Click)
 option2.addEventListener('click', option2Click)
+answerNumber2.addEventListener('click', answerNumber2Click)
 option3.addEventListener('click', option3Click)
+answerNumber3.addEventListener('click', answerNumber3Click)
 option4.addEventListener('click', option4Click)
+answerNumber4.addEventListener('click', answerNumber4Click)
 
 window.onload = sendApiRequest;
 
@@ -55,6 +63,24 @@ function option1Click(event) {
   }
 
 };
+function answerNumber1Click(event) {
+  if (currentQuestion >= maxQuestions && currentScore >= 300) {
+    youWin();
+  } else if (currentQuestion >= maxQuestions && currentScore < 250) {
+    youLose();
+  } else if (event.target === answerNumber1) {
+    gameCurrentQuestion.innerHTML = `Correct!`;
+    currentQuestion++;
+    questionCounter.innerHTML = currentQuestion;
+    currentScore = currentScore + points;
+    score.innerHTML = currentScore;
+    setTimeout(function () {
+      sendApiRequest();
+    }, 1000);
+
+  }
+
+};
 
 function option2Click(event) {
   if (currentQuestion >= maxQuestions && currentScore >= 300) {
@@ -71,6 +97,23 @@ function option2Click(event) {
   }
 }
 
+function answerNumber2Click(event) {
+  if (currentQuestion >= maxQuestions && currentScore >= 300) {
+    youWin();
+  } else if (currentQuestion >= maxQuestions && currentScore < 250) {
+    youLose();
+  } else if (event.target === answerNumber2) {
+    gameCurrentQuestion.innerHTML = `Sorry, incorrect!`;
+    currentQuestion++;
+    questionCounter.innerHTML = currentQuestion;
+    score.innerHTML = currentScore;
+    setTimeout(function () {
+      sendApiRequest();
+    }, 1000);
+
+  }
+
+};
 
 function option3Click(event) {
   if (currentQuestion >= maxQuestions && currentScore >= 300) {
@@ -87,6 +130,23 @@ function option3Click(event) {
   }
 };
 
+function answerNumber3Click(event) {
+  if (currentQuestion >= maxQuestions && currentScore >= 300) {
+    youWin();
+  } else if (currentQuestion >= maxQuestions && currentScore < 250) {
+    youLose();
+  } else if (event.target === answerNumber3) {
+    gameCurrentQuestion.innerHTML = `Sorry, incorrect!`;
+    currentQuestion++;
+    questionCounter.innerHTML = currentQuestion;
+    score.innerHTML = currentScore;
+    setTimeout(function () {
+      sendApiRequest();
+    }, 1000);
+
+  }
+
+};
 
 function option4Click(event) {
   if (currentQuestion >= maxQuestions && currentScore >= 300) {
@@ -103,10 +163,29 @@ function option4Click(event) {
   }
 }
 
+function answerNumber4Click(event) {
+  if (currentQuestion >= maxQuestions && currentScore >= 300) {
+    youWin();
+  } else if (currentQuestion >= maxQuestions && currentScore < 250) {
+    youLose();
+  } else if (event.target === answerNumber4) {
+    gameCurrentQuestion.innerHTML = `Sorry, incorrect!`;
+    currentQuestion++;
+    questionCounter.innerHTML = currentQuestion;
+    score.innerHTML = currentScore;
+    setTimeout(function () {
+      sendApiRequest();
+    }, 1000);
+
+  }
+
+};
+
 
 
 function youWin(){
       modal.classList.remove('hidden');
+  modalText.textContent = `Congratulations you have won!!!`;
       gameCurrentQuestion.innerHTML = ` `;
 
    };
